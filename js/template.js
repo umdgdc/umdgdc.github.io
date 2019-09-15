647c797baa1f464babc3817ff85dbb26a85eea6e
Joseph Feldmann <josfeldmann@gmail.com>
2019-09-15 15:45:35 -0400

    made front page more minimal

--- js/template.js
+++ js/template.js
@@ -1,5 +1,4 @@
 
-
 function makeNavbar(){
 	document.getElementById("navarea").innerHTML = document.getElementById("navarea").innerHTML = "<nav class='navbar navbar-inverse'> <a href='index.html' class='navbar-left'><img src='images/logo.png'></a><div class='container-fluid'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>Menu</button><a class='navbar-brand' href='index.html'>UMD GDC</a></div>      <div class='collapse navbar-collapse float_right' id='myNavbar'>        <ul class='nav navbar-nav'>       <li><a href='index.html'>Home</a></li>     <li><a href='https://calendar.google.com/calendar?cid=dWRxaG85NWdoZm5kNDRkNnA4aDRrc2RoYXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'>Calendar</a></li>     <li><a href='games.html'>Games</a></li>                    <li><a href='about.html'>About</a></li>          <li><a href='index.html#contact'>Contact</a></li></ul></div></div></nav>";
 
@@ -9,6 +8,7 @@ function makeNavbar(){
 function makeNavbarFolder(){
 	document.getElementById("navarea").innerHTML = document.getElementById("navarea").innerHTML = "<nav class='navbar navbar-inverse'> <a href='../index.html' class='navbar-left'><img src='../images/logo.png'></a><div class='container-fluid'><div class='navbar-header'><button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>Menu</button><a class='navbar-brand' href='../index.html'>UMD GDC</a></div>      <div class='collapse navbar-collapse float_right' id='myNavbar'>        <ul class='nav navbar-nav'>       <li><a href='../index.html'>Home</a></li>     <li><a href='../events.html'>Events</a></li>     <li><a href='../games.html'>Games</a></li>                   <li><a href='../about.html'>About</a></li>          <li><a href='../index.html#contact'>Contact</a></li></ul></div></div></nav>";
 }
+
 
 function makeFooter(){
 
